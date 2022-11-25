d2a313690a0bc6f436383b798877fb23e86e644a
Daniel Ik <codingeinstein@gmail.com>
2022-11-25 01:47:41 +0100

    update storage path

--- src/data.py
+++ src/data.py
@@ -2,14 +2,14 @@ from torchvision.datasets import ImageNet
 
 def get_imagetnet_dataset(train_transform=None, test_transform=None):
     if train_transform:
-        train = ImageNet(root='data', split='train', transform=train_transform)
+        train = ImageNet(root='data/imagenet', split='train', transform=train_transform)
     else:
-        train = ImageNet(root='data', split='train')
+        train = ImageNet(root='data/imagenet', split='train')
 
     if test_transform:
-        test = ImageNet(root='data', split='val', transform=test_transform)
+        test = ImageNet(root='data/imagenet', split='val', transform=test_transform)
     else:
-        test = ImageNet(root='data', split='val')
+        test = ImageNet(root='data/imagenet', split='val')
 
 
     return train, test
